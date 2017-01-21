212a3523157e93fce05fc32a6788578cefa6829d
Simon Moniere Abes <simon.moniereabes@gmail.com>
2017-01-21 11:13:23 -0500

    Added camera

--- Clarifind/app/src/main/java/com/conuhax/clarifind/CameraActivity.java
+++ Clarifind/app/src/main/java/com/conuhax/clarifind/CameraActivity.java
@@ -3,11 +3,26 @@ package com.conuhax.clarifind;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sandrios.sandriosCamera.internal.SandriosCamera;
+import com.sandrios.sandriosCamera.internal.configuration.CameraConfiguration;
+
 public class CameraActivity extends AppCompatActivity {
+
+    private static final int CAPTURE_MEDIA = 368;
+    private boolean showImagePicker = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        launchCamera();
+    }
+
+    private void launchCamera() {
+        new SandriosCamera(CameraActivity.this, CAPTURE_MEDIA)
+                .setShowPicker(showImagePicker)
+                .setVideoFileSize(15) //File Size in MB: Default is no limit
+                .setMediaAction(CameraConfiguration.MEDIA_ACTION_PHOTO) // default is CameraConfiguration.MEDIA_ACTION_BOTH
+                .launchCamera();
     }
 }
