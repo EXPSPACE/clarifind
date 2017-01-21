948c7f0fd0dd7f21f9b7d0cc59884a7cb6b4646f
Simon Moniere Abes <simon.moniereabes@gmail.com>
2017-01-21 11:53:38 -0500

    Converts the picture to bitmap

--- Clarifind/app/src/main/java/com/conuhax/clarifind/CameraActivity.java
+++ Clarifind/app/src/main/java/com/conuhax/clarifind/CameraActivity.java
@@ -1,7 +1,13 @@
 package com.conuhax.clarifind;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.sandrios.sandriosCamera.internal.SandriosCamera;
 import com.sandrios.sandriosCamera.internal.configuration.CameraConfiguration;
@@ -18,6 +24,25 @@ public class CameraActivity extends AppCompatActivity {
         launchCamera();
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAPTURE_MEDIA && resultCode == RESULT_OK) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            String s = "File " + data.getStringExtra(CameraConfiguration.Arguments.FILE_PATH);
+            String picPath = data.getStringExtra(CameraConfiguration.Arguments.FILE_PATH);
+
+
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            Bitmap bitmap = BitmapFactory.decodeFile(picPath,bmOptions);
+            bitmap = Bitmap.createScaledBitmap(bitmap,1000,1000,true);
+            imageView.setImageBitmap(bitmap);
+
+            Log.e("File", "" + data.getStringExtra(CameraConfiguration.Arguments.FILE_PATH));
+            Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void launchCamera() {
         new SandriosCamera(CameraActivity.this, CAPTURE_MEDIA)
                 .setShowPicker(showImagePicker)
@@ -25,4 +50,6 @@ public class CameraActivity extends AppCompatActivity {
                 .setMediaAction(CameraConfiguration.MEDIA_ACTION_PHOTO) // default is CameraConfiguration.MEDIA_ACTION_BOTH
                 .launchCamera();
     }
+
+
 }
